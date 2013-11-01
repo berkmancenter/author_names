64d70acf76dd456b610f9281fde950bc2b0abaa2
Anita Patel <anita@anitapatel.com>
2013-11-01 16:04:53 -0400

    removed limit 2 for country

--- db/migrate/20130624155059_create_publishers.rb
+++ db/migrate/20130624155059_create_publishers.rb
@@ -11,7 +11,7 @@ class CreatePublishers < ActiveRecord::Migration
       t.string :city, :limit => 100, :null => false
       t.string :state, :limit => 100, :null => false
       t.string :postal_code, :limit => 30, :null => false
-      t.string :country, :limit => 2, :null => false
+      t.string :country, :null => false
       t.string :website
       t.timestamps
     end
