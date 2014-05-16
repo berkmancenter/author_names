44911b53ba854202bd3e1f05ef66c3e586248e3d
Anita Patel <anita@anitapatel.com>
2014-05-16 11:40:28 -0400

    bug fix

--- app/controllers/form_items_controller.rb
+++ app/controllers/form_items_controller.rb
@@ -24,7 +24,7 @@ class FormItemsController < ApplicationController
       @questionnaires = Questionnaire.find(@questionnaire_ids)
       @q_links = ""
       @questionnaires.each do |q|
-        @q_links = @q_links + "<a href='http://#{ROOT_URL}#{questionnaire_path(q)}'>#{q.name}</a><br />"
+        @q_links = @q_links + "<a href='#{ROOT_URL}#{questionnaire_path(q)}'>#{q.name}</a><br />"
       end  
       flash[:notice] = "<p>Note these questionnaires are using this form item: <br />#{@q_links}</p>".html_safe
     end
