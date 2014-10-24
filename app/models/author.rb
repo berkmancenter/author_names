853a5b740b33240e0c40ac9074e92e52604b1285
Anita Patel <anita@anitapatel.com>
2014-10-23 22:35:25 -0400

    forcing user_id for Author creation

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,7 +5,7 @@ class Author < ActiveRecord::Base
   belongs_to :user
   has_many :publications, :dependent => :destroy
   
-  validates_presence_of :publisher_id
+  validates_presence_of :publisher_id, :user_id
   
   def to_s
     self.full_name
